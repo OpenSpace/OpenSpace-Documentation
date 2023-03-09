619e27d53d120aee66445c0d4d7e170a17c9534e
Ylva Selling <ylva.selling@gmail.com>
2023-03-09 14:07:20 -0500

    Use local storage to store dark or light mode

--- src/ColorModeContext.jsx
+++ src/ColorModeContext.jsx
@@ -3,12 +3,51 @@ import useMediaQuery from '@mui/material/useMediaQuery';
 
 const ColorModeContext = React.createContext()
 
+function useLocalStorageState(
+    key,
+    defaultValue = '',
+    // the = {} fixes the error we would get from destructuring when no argument was passed
+    // Check https://jacobparis.com/blog/destructure-arguments for a detailed explanation
+    {serialize = JSON.stringify, deserialize = JSON.parse} = {},
+  ) {
+    const [state, setState] = React.useState(() => {
+      const valueInLocalStorage = window.localStorage.getItem(key);
+      if (valueInLocalStorage) {
+        // the try/catch is here in case the localStorage value was set before
+        // we had the serialization in place (like we do in previous extra credits)
+        try {
+          return deserialize(valueInLocalStorage);
+        } catch (error) {
+          window.localStorage.removeItem(key);
+        }
+      }
+      return (typeof defaultValue === 'function') ? defaultValue() : defaultValue;
+    })
+  
+  const prevKeyRef = React.useRef(key);
+  
+    // Check the example at src/examples/local-state-key-change.js to visualize a key change
+  React.useEffect(() => {
+    const prevKey = prevKeyRef.current;
+    if (prevKey !== key) {
+      window.localStorage.removeItem(prevKey);
+    }
+    prevKeyRef.current = key;
+    window.localStorage.setItem(key, serialize(state));
+  }, [key, state, serialize]);
+  
+  return [state, setState];
+}
+
 function ColorModeProvider(props) {
-  const [mode, setMode] = React.useState('dark');
+  const [mode, setMode] = useLocalStorageState('colorMode', '');
   const initialMode = useMediaQuery('(prefers-color-scheme: dark)', { noSsr: true }) ? 'dark' : 'light';
 
   React.useEffect(() => {
-    setMode(initialMode);
+    if (mode === '') {
+      // If there is no local storage, see what the browser wants
+      setMode(initialMode);
+    }
   }, []); 
   
   const colorMode = React.useMemo(
