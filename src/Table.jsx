cf229ecb130ce0891904fbca1ca52d6e1ef23797
Ylva Selling <ylva.selling@gmail.com>
2023-03-16 15:08:40 -0400

    Add copy button to URI

--- src/Table.jsx
+++ src/Table.jsx
@@ -7,6 +7,8 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 function CellLink({onClick, row, name}) {
   return (
@@ -24,6 +26,28 @@ function CellLink({onClick, row, name}) {
     </TableCell>
   );
 }
+
+function CopyUriButton({ uri }) {
+  function color(theme) {
+    return theme.palette.secondary[theme.palette.mode];
+  }
+
+  return (
+      <Button
+        component="button"
+        variant="body2"
+        onClick={() => navigator.clipboard.writeText(uri)}
+        sx={{
+          marginLeft: '10px',
+          padding: '3px 10px', 
+          backgroundColor: color,
+        }}
+      >
+        {"Copy URI"}
+      </Button>
+  );
+}
+  
   
 export default function BasicTable({ headers, rows, setSelectedItem, cellFunc }) {
   if (!(rows?.length && rows?.length > 0)) {
@@ -51,10 +75,22 @@ export default function BasicTable({ headers, rows, setSelectedItem, cellFunc })
                   if (header === 'name') {
                     return null;
                   }
+                  if (header === 'URI') {
+                    return (
+                      <>
+                        <TableCell>
+                          <Box sx={{ display:'flex', alignItems: 'center' }}>
+                            {row[header]}
+                            <CopyUriButton uri={row.URI} />
+                          </Box>
+                        </TableCell>
+                      </>
+                    );
+                  }
                   if (cellFunc && cellFunc.Name === header) {
                     return <CellLink onClick={cellFunc.Function} row={row} name={row[cellFunc.Name]} />;
                   }
-                  return !Array.isArray(row[header]) && <TableCell>{row[header]}</TableCell>
+                  return !Array.isArray(row[header]) && <TableCell>{row[header]}</TableCell>;
                 })}
               </TableRow>
             </>
